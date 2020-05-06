cb9f746b8f65b4945f855aa7ca98a3c612873d78
Aprila Rachmat Riady <aprila.rachmat@gmail.com>
2020-05-06 15:31:01 +0700

    mengahpus kata PT

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    PT Geoservices Bandung 
+                Geoservices Bandung 
                 </div>
 
                 <div class="links">
